965626251c38aec56275ccb79a72758a3d747bc2
guptajatin <gupta04jatin@gmail.com>
2025-03-13 16:56:39 +0530

    updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,6 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hello, World!!!!\n This is updated one";
   return 0;
 }
